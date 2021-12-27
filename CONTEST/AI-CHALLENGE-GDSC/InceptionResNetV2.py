9e94707d49764195a0651b57034acc7cceee9e04
Louis2602 <trantunglam135790@gmail.com>
2021-12-27 19:32:37 +0700

    add new files

--- CONTEST/AI-CHALLENGE-GDSC/InceptionResNetV2.py
+++ CONTEST/AI-CHALLENGE-GDSC/InceptionResNetV2.py
@@ -10,7 +10,7 @@ from keras.models import Sequential
 from keras.utils import np_utils
 from keras.models import load_model
 from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator                
+from keras.preprocessing.image import ImageDataGenerator
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,40 +20,44 @@ import pandas as pd
 # Reading the data
 training_csv = pd.read_csv('../input/gdscaidataset/trainLabels.csv')
 
-mappings, training_labels = np.unique(training_csv['label'].to_numpy(copy=True), return_inverse=True)
+mappings, training_labels = np.unique(
+    training_csv['label'].to_numpy(copy=True), return_inverse=True)
 training_labels = training_labels.reshape(-1, 1)
 
-training_images = np.array([plt.imread(f'../input/gdscaidataset/train/train/{i + 1}.png') for i in range(0, 50000)])
-testing_images  = np.array([plt.imread(f'../input/gdscaidataset/test/test/{i + 1}.png')   for i in range(0, 20000)])
+training_images = np.array([plt.imread(
+    f'../input/gdscaidataset/train/train/{i + 1}.png') for i in range(0, 50000)])
+testing_images = np.array([plt.imread(
+    f'../input/gdscaidataset/test/test/{i + 1}.png') for i in range(0, 20000)])
 
 # Import RestNet50 or InceptionResNetV2 to train
 #conv_base = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-conv_base = tf.keras.applications.inception_resnet_v2.InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+conv_base = tf.keras.applications.inception_resnet_v2.InceptionResNetV2(
+    weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
 conv_base.summary()
 
 # calculate images size and retype it
 training_images = training_images.astype('float32')
 testing_images = testing_images.astype('float32')
-                                       
-#z-score
-mean = np.mean(training_images,axis=(0,1,2,3))
-std = np.std(training_images,axis=(0,1,2,3))
+
+# z-score
+mean = np.mean(training_images, axis=(0, 1, 2, 3))
+std = np.std(training_images, axis=(0, 1, 2, 3))
 training_images = (training_images-mean)/(std+1e-7)
 testing_images = (testing_images-mean)/(std+1e-7)
 
 training_labels = np_utils.to_categorical(training_labels, 10)
 
-print("training_images shape:",training_images.shape)
-print("testing_images shape:",testing_images.shape)
-                                       
-baseMapNum = 32
+print("training_images shape:", training_images.shape)
+print("testing_images shape:", testing_images.shape)
+
+baseMapNum = 2
 weight_decay = 1e-4
 
 # Add layers
 
 model = models.Sequential()
-model.add(layers.UpSampling2D((7,7)))
+model.add(layers.UpSampling2D((7, 7)))
 model.add(conv_base)
 
 model.add(layers.Flatten())
@@ -65,17 +69,17 @@ model.add(layers.Flatten())
 # model.add(layers.Dropout(0.5))
 # model.add(layers.BatchNormalization())
 
-model.add(Dense(4096, activation='elu', kernel_constraint=maxnorm(3))) 
+model.add(Dense(4096, activation='elu', kernel_constraint=maxnorm(3)))
 model.add(BatchNormalization())
-model.add(Dropout(0.5)) 
+model.add(Dropout(0.5))
 
-model.add(Dense(4096, activation='elu', kernel_constraint=maxnorm(3))) 
+model.add(Dense(4096, activation='elu', kernel_constraint=maxnorm(3)))
 model.add(BatchNormalization())
-model.add(Dropout(0.5)) 
+model.add(Dropout(0.5))
 
-model.add(Dense(1000, activation='elu', kernel_constraint=maxnorm(3))) 
+model.add(Dense(1000, activation='elu', kernel_constraint=maxnorm(3)))
 model.add(BatchNormalization())
-model.add(Dropout(0.5)) 
+model.add(Dropout(0.5))
 
 model.add(layers.Dense(10, activation='softmax'))
 
@@ -97,36 +101,44 @@ datagen = ImageDataGenerator(
     height_shift_range=0.1,
     horizontal_flip=True,
     vertical_flip=False
-    )
+)
 datagen.fit(training_images)
 
 # Training models
 batch_size = 32
-epochs=10
-opt_rms = tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9,decay=1e-6, nesterov=False)
+epochs = 10
+opt_rms = tf.keras.optimizers.SGD(
+    learning_rate=0.001, momentum=0.9, decay=1e-6, nesterov=False)
 model.compile(loss='binary_crossentropy',
-        optimizer=opt_rms,
-        metrics=['accuracy'])
+              optimizer=opt_rms,
+              metrics=['accuracy'])
 # steps_per_epoch=training_images.shape[0]// batch_size
-model.fit(datagen.flow(training_images, training_labels, batch_size=batch_size),epochs=2*epochs,verbose=1)
+model.fit(datagen.flow(training_images, training_labels,
+          batch_size=batch_size), epochs=2*epochs, verbose=1)
 
-opt_rms = tf.keras.optimizers.SGD(learning_rate=0.0005, momentum=0.9,decay=1e-6, nesterov=False)
+opt_rms = tf.keras.optimizers.SGD(
+    learning_rate=0.0005, momentum=0.9, decay=1e-6, nesterov=False)
 model.compile(loss='binary_crossentropy',
-        optimizer=opt_rms,
-        metrics=['accuracy'])
-model.fit(datagen.flow(training_images, training_labels, batch_size=batch_size),epochs=epochs,verbose=1)
+              optimizer=opt_rms,
+              metrics=['accuracy'])
+model.fit(datagen.flow(training_images, training_labels,
+          batch_size=batch_size), epochs=epochs, verbose=1)
 
-opt_rms = tf.keras.optimizers.SGD(learning_rate=0.0003, momentum=0.9,decay=1e-6, nesterov=False)
+opt_rms = tf.keras.optimizers.SGD(
+    learning_rate=0.0003, momentum=0.9, decay=1e-6, nesterov=False)
 model.compile(loss='binary_crossentropy',
-        optimizer=opt_rms,
-        metrics=['accuracy'])
-model.fit(datagen.flow(training_images, training_labels, batch_size=batch_size),epochs=epochs,verbose=1)
+              optimizer=opt_rms,
+              metrics=['accuracy'])
+model.fit(datagen.flow(training_images, training_labels,
+          batch_size=batch_size), epochs=epochs, verbose=1)
 
-opt_rms = tf.keras.optimizers.SGD(learning_rate=0.0008, momentum=0.9,decay=1e-6, nesterov=False)
+opt_rms = tf.keras.optimizers.SGD(
+    learning_rate=0.0008, momentum=0.9, decay=1e-6, nesterov=False)
 model.compile(loss='binary_crossentropy',
-        optimizer=opt_rms,
-        metrics=['accuracy'])
-model.fit(datagen.flow(training_images, training_labels, batch_size=batch_size),epochs=epochs,verbose=1)
+              optimizer=opt_rms,
+              metrics=['accuracy'])
+model.fit(datagen.flow(training_images, training_labels,
+          batch_size=batch_size), epochs=epochs, verbose=1)
 
 # Predict the result
 prediction = model.predict(testing_images)
